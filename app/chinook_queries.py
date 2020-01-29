6fa55331a89b3f840e6b3e7ccfe86aa8d1c5059b
Mike Rossetti <s2t2@users.noreply.github.com>
2020-01-28 21:50:39 -0500

    Investigating

--- app/chinook_queries.py
+++ app/chinook_queries.py
@@ -1,16 +1,32 @@
 
 import os
 import sqlite3
+from pprint import pprint
 
 DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "chinook.db")
 
 conn = sqlite3.connect(DB_FILEPATH)
+print("CONNECTION:", type(conn)) #> <class 'sqlite3.Connection'>
 
 curs = conn.cursor()
+print("CURSOR:", type(curs)) #> <class 'sqlite3.Cursor'>
 
-query = "SELECT COUNT(customerId) FROM customers;"
-print("SQL", query)
-#curs.execute(query)
+queries = [
+    #"SELECT COUNT(customerId) FROM customers;",
+    "SELECT * FROM customers LIMIT 3;"
+]
 
-results = curs.execute(query).fetchall()
-print("RESULTS", results)
+for query in queries:
+    print("--------------")
+    print(f"QUERY: '{query}'")
+
+    #obj = curs.execute(query)
+    #print("OBJ", type(obj))
+    #print(obj) #> <class 'sqlite3.Cursor'>
+
+    results = curs.execute(query).fetchall()
+    print("RESULTS:", type(results))
+    print(results)
+
+    print(type(results[0])) #> type(results[0])
+    breakpoint()
