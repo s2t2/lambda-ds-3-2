cbd5e207f672206bdeff09fe77b6c7e865031adb
Mike Rossetti <s2t2@users.noreply.github.com>
2020-01-31 01:01:28 -0500

    Refactor to OOP

--- app/etl_pipeline.py
+++ app/etl_pipeline.py
@@ -16,53 +16,57 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
 
 DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "rpg_db.sqlite3")
 
-sqlite_connection = sqlite3.connect(DB_FILEPATH)
-#sqlite_connection.row_factory = sqlite3.Row
-sqlite_cursor = sqlite_connection.cursor()
+class StorageService():
+    def __init__(self):
+        self.sqlite_connection = sqlite3.connect(DB_FILEPATH)
+        self.sqlite_cursor = self.sqlite_connection.cursor()
+        self.pg_connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        self.pg_cursor = self.pg_connection.cursor()
 
-pg_connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-pg_cursor = pg_connection.cursor()
+    def get_characters(self):
+        return self.sqlite_connection.execute("SELECT * FROM charactercreator_character;").fetchall()
 
-def create_characters_table(pg_connection, pg_cursor):
-    create_query = """
-    CREATE TABLE IF NOT EXISTS characters (
-        character_id SERIAL PRIMARY KEY,
-        name VARCHAR(30),
-        level INT,
-        exp INT,
-        hp INT,
-        strength INT,
-        intelligence INT,
-        dexterity INT,
-        wisdom INT
-    )
-    """
-    print(create_query)
-    pg_cursor.execute(create_query)
-    pg_connection.commit()
+    def create_characters_table(self):
+        create_query = """
+        DROP TABLE IF EXISTS characters; -- allows this to be run idempotently, avoids psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "characters_pkey" DETAIL:  Key (character_id)=(1) already exists.
+        CREATE TABLE IF NOT EXISTS characters (
+            character_id SERIAL PRIMARY KEY,
+            name VARCHAR(30),
+            level INT,
+            exp INT,
+            hp INT,
+            strength INT,
+            intelligence INT,
+            dexterity INT,
+            wisdom INT
+        );
+        """
+        print(create_query)
+        self.pg_cursor.execute(create_query)
+        self.pg_connection.commit()
 
-def insert_characters(pg_connection, pg_cursor, characters):
-    insertion_query = "INSERT INTO characters (character_id, name, level, exp, hp, strength, intelligence, dexterity, wisdom) VALUES %s"
-    #records = df.to_dict("records") #> [{0: 'A rowwwww', 1: 'null'}, {0: 'Another row, with JSONNNNN', 1: '{"a": 1, "b": ["dog", "cat", 42], "c": "true"}'}, {0: 'Third row', 1: '3'}, {0: 'Pandas Row', 1: 'YOOO!'}]
-    #list_of_tuples = [(r[0], r[1]) for r in records]
-    list_of_tuples = characters
-    execute_values(pg_cursor, insertion_query, list_of_tuples)
-    pg_connection.commit()
+    def insert_characters(self, characters):
+        insertion_query = "INSERT INTO characters (character_id, name, level, exp, hp, strength, intelligence, dexterity, wisdom) VALUES %s"
+        list_of_tuples = characters
+        execute_values(self.pg_cursor, insertion_query, list_of_tuples)
+        self.pg_connection.commit()
 
 if __name__ == "__main__":
 
+    service = StorageService()
+
     #
-    # EXTRACT
+    # EXTRACT AND TRANSFORM
     #
 
-    characters = sqlite_connection.execute("SELECT * FROM charactercreator_character;").fetchall()
-    print(type(characters))
-    print(characters)
+    characters = service.get_characters()
+    print(type(characters), len(characters))
+    print(characters[0])
 
     #
     # LOAD
     #
 
-    create_characters_table(pg_connection, pg_cursor)
+    service.create_characters_table()
 
-    insert_characters(pg_connection, pg_cursor, characters)
+    service.insert_characters(characters)
