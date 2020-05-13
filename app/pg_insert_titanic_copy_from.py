76ec2477d6e407d9d5c6bd82636f53ec5cbf06ab
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-13 14:42:49 -0400

    Create pg_insert_titanic_copy_from.py

---
+++ app/pg_insert_titanic_copy_from.py
@@ -0,0 +1,51 @@
+
+# shared by Aaron from DS 14
+
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PW,
+    host=DB_HOST)
+cur = conn.cursor()
+
+# Review shape of titanic.csv in terminal
+# to determine schema
+# 8 columns:
+"""
+Survived INT
+Pclass INT
+Name VARCHAR(40) NOT NULL
+Sex ENUM
+Age INT
+Siblings/Spouses Aboard INT
+Parents/Children Aboard INT
+Fare NUMERIC
+"""
+
+# Create titanic table
+# cur.execute(
+#     """
+#     --CREATE TYPE sex AS ENUM ('male', 'female');
+#     CREATE TABLE titanic (
+#         Survived INTEGER NOT NULL,
+#         Pclass INTEGER NOT NULL,
+#         Name TEXT NOT NULL,
+#         Sex sex,
+#         Age NUMERIC NOT NULL,
+#         "Siblings/Spouses Aboard" INTEGER NOT NULL,
+#         "Parents/Children Aboard" INTEGER NOT NULL,
+#         Fare NUMERIC NOT NULL
+#     );
+#     """
+# )
+# conn.commit()
+
+### Insert titanic data
+### https://www.dataquest.io/blog/loading-data-into-postgres/
+
+with open('module2-sql-for-analysis/titanic.csv', 'r') as f:
+    next(f)  # Skip header row
+    cur.copy_from(f, 'titanic', sep=',')
+ 
+conn.commit()
+
